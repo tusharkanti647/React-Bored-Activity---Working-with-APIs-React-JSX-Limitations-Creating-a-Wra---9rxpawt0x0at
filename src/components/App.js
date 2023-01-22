643a5d69c66be97e3d24348c6ab1309a1ddc093b
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-22 19:56:03 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+
+const Loader = () => <div id="loader">Loading...</div>
+
+
+const App = () => {
+
+  
+
+  return (
+    <div id="main">
+    
+    </div>
+  )
+}
+
+
+export default App;
